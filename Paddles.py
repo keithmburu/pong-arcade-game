3c00f9f920fc85cf2ee5033956bd4cd797799f98
kmburu <keithmburu1@gmail.com>
2022-05-30 14:53:58 +0300

    edit

--- Paddles.py
+++ Paddles.py
@@ -7,7 +7,7 @@ class Paddle(pygame.sprite.Sprite):
 
    def __init__(self, color, width, height):
        # to initialize the main properties of the object
-       super().__init__()
+       super().__init__ ()
 
        # calling the "Sprite" class constructor
        self.image = pygame.Surface([width, height])
@@ -30,7 +30,7 @@ class Paddle(pygame.sprite.Sprite):
 
 
    def moveDown(self, pixels):# this method is used to move the paddles down by changing their pixels which are also their positions
-       
+
        self.rect.y += pixels #while the paddles are moving down, only their y-component pixels will increase
        if self.rect.y > 450: # the following section of code make sures that paddles will not move outside of bottom of the screen
            self.rect.y = 450
